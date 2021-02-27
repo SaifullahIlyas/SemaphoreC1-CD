6f98f7459d9f847cb744ed3b1839ae8a7dbcc140
Saifullah ilyas <saifullahilyas907@gmail.com>
2021-02-27 21:56:21 +0500

    unit test added for outlet setup for vc and tableview single section

---
+++ bestpracticesTests/App/Views/Trending/Controllers/TrendingRepoViewControllerTests.swift
@@ -0,0 +1,44 @@
+//
+//  TrendingRepoViewControllerTests.swift
+//  bestpracticesTests
+//
+//  Created by SAIF ULLAH on 27/02/2021.
+//
+
+import XCTest
+@testable import bestpractices
+
+class TrendingRepoViewControllerTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testOutlestSetupForVC(){
+        let vc = TrendingReposVC()
+        vc.loadView()
+        XCTAssert(vc.tableView != nil)
+    }
+    func testTableViewHaveOneSection(){
+        let vc = TrendingReposVC()
+        vc.loadView()
+        XCTAssert(vc.tableView?.numberOfSections == 1)
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
