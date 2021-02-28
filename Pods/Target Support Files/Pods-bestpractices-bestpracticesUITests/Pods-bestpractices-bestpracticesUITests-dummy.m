0c7abe5893f53473cff8f8cfeaa4be64a6679e8f
Saifullah ilyas <saifullahilyas907@gmail.com>
2021-02-28 20:07:24 +0500

    pod file removed from gitignore to test CI

---
+++ Pods/Target Support Files/Pods-bestpractices-bestpracticesUITests/Pods-bestpractices-bestpracticesUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_bestpractices_bestpracticesUITests : NSObject
+@end
+@implementation PodsDummy_Pods_bestpractices_bestpracticesUITests
+@end
---
+++ Pods/Target Support Files/Pods-bestpracticesTests/Pods-bestpracticesTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_bestpracticesTests : NSObject
+@end
+@implementation PodsDummy_Pods_bestpracticesTests
+@end
